6f32e0c679bf6052ad273efe783eafdef1556649
serraguti <pacoserranox@gmail.com>
2023-02-08 10:05:04 +0100

    commit

--- MvcCoreAdoNet/Controllers/HospitalesController.cs
+++ MvcCoreAdoNet/Controllers/HospitalesController.cs
@@ -24,5 +24,22 @@ namespace MvcCoreAdoNet.Controllers
             Hospital hospital = this.repo.FindHospital(idhospital);
             return View(hospital);
         }
+
+        public IActionResult CreateHospital()
+        {
+            return View();
+        }
+
+        //CUANDO PULSEMOS EL BOTON, QUE DEBEMOS RECIBIR???
+        //MEJOR RECIBIR UN MODEL BINDING
+        [HttpPost]
+        public IActionResult CreateHospital(Hospital hospital)
+        {
+            this.repo.CreateHospital(hospital.IdHospital
+                , hospital.Nombre, hospital.Direccion
+                , hospital.Telefono, hospital.Camas);
+            ViewData["MENSAJE"] = "Hospital insertado";
+            return View();
+        }
     }
 }
